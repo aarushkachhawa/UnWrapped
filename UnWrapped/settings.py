396f34be323e30717933ace4880b7cefdba77d96
anyakhosla <anyakhosla05@gmail.com>
2024-10-16 16:14:36 -0400

    test push

--- UnWrapped/settings.py
+++ UnWrapped/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
+
+test push
 """
 
 from pathlib import Path
