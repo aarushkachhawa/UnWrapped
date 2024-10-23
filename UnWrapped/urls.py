cf9f62901f1742d235eb8120505003beb067bcb9
aarushkachhawa <aarushkachhawa@gmail.com>
2024-10-23 16:27:31 -0400

    Got Spotify API credentials to work with redirect back to localhost.

--- UnWrapped/urls.py
+++ UnWrapped/urls.py
@@ -1,33 +1,17 @@
-"""
-URL configuration for UnWrapped project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from . import views
 from django.shortcuts import redirect
 
-
 app_name = 'UnWrapped'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: redirect('login', permanent=True)),
+    path('', lambda request: redirect('login', permanent=True)),  # Redirect root to login
     path('register/', views.register, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('login/', views.login_view, name='login'),
     path('home/', views.home, name='home'),
     path('stats/', views.stats, name='stats'),
+    path('spotify_callback/', views.spotify_callback, name='spotify_callback'),  # Spotify OAuth callback
 ]
