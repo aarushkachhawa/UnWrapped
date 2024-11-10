5b55cf5b3c3fa7c656e40ec5e7e794b7ac856792
aditikoratp <aditikoratp@gmail.com>
2024-11-09 19:38:12 -0500

    modified

--- UnWrapped/urls.py
+++ UnWrapped/urls.py
@@ -7,11 +7,16 @@ app_name = 'UnWrapped'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: redirect('UnWrapped:login', permanent=True)),  # Updated with namespace
+    path('', lambda request: redirect('login', permanent=True)),  # Redirect root to login
     path('register/', views.register, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('login/', views.login_view, name='login'),
     path('home/', views.home, name='home'),
     path('stats/', views.stats, name='stats'),
-    path('spotify_callback/', views.spotify_callback, name='spotify_callback'),
+    path('spotify_callback/', views.spotify_callback, name='spotify_callback'),  # Spotify OAuth callback
+    path('calculate_ads/', views.calculate_ads, name='calculate_ads'),
+    path('top_artists/', views.get_most_popular_artists, name="top_artists"),
+    path('analyze_seasonal_mood/', views.analyze_seasonal_mood, name="analyze_seasonal_mood"),
+    path('analyze_clothing/', views.analyze_clothing, name="analyze_clothing"),
+    path('night_owl/', views.night_owl, name="night_owl"),
 ]
