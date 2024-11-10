964c6fe1fad4272d2e782d0f2315c8a7f8ce1406
aditikoratp <aditikoratp@gmail.com>
2024-11-10 15:03:39 -0500

    modified

--- UnWrapped/urls.py
+++ UnWrapped/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path('analyze_seasonal_mood/', views.analyze_seasonal_mood, name="analyze_seasonal_mood"),
     path('analyze_clothing/', views.analyze_clothing, name="analyze_clothing"),
     path('night_owl/', views.night_owl, name="night_owl"),
+    path('transition_one/', views.transition_one, name="transition_one"),
 ]
