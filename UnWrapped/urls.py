2c6c46967d956382cdfdbd12fefd20154c7de62b
aditikoratp <aditikoratp@gmail.com>
2024-11-09 19:00:27 -0500

    created UI for register and login

--- UnWrapped/urls.py
+++ UnWrapped/urls.py
@@ -7,11 +7,11 @@ app_name = 'UnWrapped'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: redirect('login', permanent=True)),  # Redirect root to login
+    path('', lambda request: redirect('UnWrapped:login', permanent=True)),  # Updated with namespace
     path('register/', views.register, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('login/', views.login_view, name='login'),
     path('home/', views.home, name='home'),
     path('stats/', views.stats, name='stats'),
-    path('spotify_callback/', views.spotify_callback, name='spotify_callback'),  # Spotify OAuth callback
+    path('spotify_callback/', views.spotify_callback, name='spotify_callback'),
 ]
