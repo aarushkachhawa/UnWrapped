b34d814cd3d22bebae83bdc0e4b5dc0ae887af58
aditikoratp <aditikoratp@gmail.com>
2024-11-10 15:28:01 -0500

    created transition one page.

--- UnWrapped/urls.py
+++ UnWrapped/urls.py
@@ -12,13 +12,13 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('login/', views.login_view, name='login'),
     path('home/', views.home, name='home'),
-    path('stats/', views.stats, name='stats'),
+    path('top_artist/', views.top_artist_and_songs_slide, name='top_artist'),
     path('spotify_callback/', views.spotify_callback, name='spotify_callback'),  # Spotify OAuth callback
     path('calculate_ads/', views.calculate_ads, name='calculate_ads'),
     path('top_artists/', views.get_most_popular_artists, name="top_artists"),
     path('analyze_seasonal_mood/', views.analyze_seasonal_mood, name="analyze_seasonal_mood"),
     path('analyze_clothing/', views.analyze_clothing, name="analyze_clothing"),
     path('night_owl/', views.night_owl, name="night_owl"),
+    path('llm_insights_page/', views.llm_insights_page, name="llm_insights_page"),
     path('transition_one/', views.transition_one, name="transition_one"),
-
 ]
