589eed8a72894ffb8919937482a499767e9e85b0
Anonymoustank <pkadekodi@gmail.com>
2024-11-29 17:28:25 -0500

    if wrapped isnt generated, fallback to generating one

--- UnWrapped/views.py
+++ UnWrapped/views.py
@@ -248,6 +248,8 @@ def spotify_callback(request):
 
 @login_required
 def stats(request):
+    if 'generatedWrap' not in request.session or not request.session['generatedWrap']:
+        return fallback(request)
     language = request.session.get('language', 'english')
     if 'spotify_access_token' not in request.session:
         return redirect(spotify_auth_url())
@@ -299,6 +301,8 @@ def calculate_top_artist_and_songs_slide(request):
 
 
 def top_artist_and_songs_slide(request, page='topArtistAndSongs.html', extra_context=None):
+    if 'generatedWrap' not in request.session or not request.session['generatedWrap']:
+        return fallback(request)
     context = {
         'top_artist': request.session['top_artist'],
         'top_songs': request.session['top_songs'],
@@ -519,6 +523,8 @@ def calculate_get_most_popular_artists(request):
 
 
 def get_most_popular_artists(request, page='slide_2.html', extra_context=None):
+    if 'generatedWrap' not in request.session or not request.session['generatedWrap']:
+        return fallback(request)
     language = request.session.get('language', 'english')
     
     time_labels = {
@@ -726,6 +732,8 @@ def calculate_analyze_seasonal_mood(request):
     request.session['season'] = curr_season
 
 def analyze_seasonal_mood(request, page='seasonalMood.html', extra_context=None):
+    if 'generatedWrap' not in request.session or not request.session['generatedWrap']:
+        return fallback(request)
     context = {
         "mood1": request.session['mood1'],
         "mood2": request.session['mood2'],
@@ -775,6 +783,8 @@ def calculate_llm_insights_page(request):
 
 @login_required
 def llm_insights_page(request, page='LLMinsights.html', extra_context=None):
+    if 'generatedWrap' not in request.session or not request.session['generatedWrap']:
+        return fallback(request)
     context = {
         'content': request.session['content'],
         'mood': request.session['mood'],
@@ -894,6 +904,8 @@ def calculate_night_owl(request):  # combine this into one calculate stats metho
     request.session['minute_hand_rotation'] = minute_hand_rotation - 90
 
 def night_owl(request, page = 'slide_3.html', extra_content = None):
+    if 'generatedWrap' not in request.session or not request.session['generatedWrap']:
+        return fallback(request)
     language = request.session.get('language', 'english')
     context = {
         "latest_time": request.session['latest_time'],
@@ -1010,6 +1022,8 @@ def calculate_get_account_level(request):
     request.session['ads_minutes'] = round(calculate_ads(request))
 
 def get_account_level(request, page='ads_minutes.html', extra_context=None):
+    if 'generatedWrap' not in request.session or not request.session['generatedWrap']:
+        return fallback(request)
     language = request.session.get('language', 'english')
     context = {
         "premium": request.session['premium'],
@@ -1034,6 +1048,9 @@ def generate_wrap(request):
     calculate_analyze_seasonal_mood(request)
     calculate_llm_insights_page(request)
     calculate_night_owl(request)
+
+    if 'holiday' not in request.session:
+        request.session['holiday'] = 'none'
 
     # save to model
     wrap = CustomWrap(
@@ -1072,6 +1089,8 @@ def generate_wrap(request):
         holiday = request.session['holiday'],
     )
     wrap.save()
+
+    request.session['generatedWrap'] = True
 
     return JsonResponse({
         "done": True
@@ -1135,6 +1154,8 @@ def christmas_llm(request):
     return llm_insights_page(request, 'christmas_llm.html', context)
 
 def past_wraps(request):
+    if 'generatedWrap' not in request.session or not request.session['generatedWrap']:
+        return fallback(request)
     month_to_word_dict = {
         1: "Jan",
         2: "Feb",
@@ -1180,6 +1201,8 @@ def past_wraps(request):
     return render(request, 'past_wraps.html', context)
 
 def game_mix_pitch_1(request):
+    if 'generatedWrap' not in request.session or not request.session['generatedWrap']:
+        return fallback(request)
     language = request.session.get('language', 'english')
     access_token = request.session.get('spotify_access_token')
 
@@ -1293,6 +1316,8 @@ def game_mix_pitch_1(request):
 
 
 def game_mix_pitch_2(request):
+    if 'generatedWrap' not in request.session or not request.session['generatedWrap']:
+        return fallback(request)
     language = request.session.get('language', 'english')
     access_token = request.session.get('spotify_access_token')
 
@@ -1538,4 +1563,14 @@ def submit_feedback(request):
 def set_theme_from_profile(request):
     request.session['holiday'] = request.session['todays_theme']
     return redirect('slide_2')
+
+@login_required
+def fallback(request):
+    if 'holiday' not in request.session:
+        return transition_one(request)
+    elif request.session['holiday'] == 'christmas':
+        return christmas_transition_one(request)
+    elif request.session['holiday'] == 'halloween':
+        return halloween_transition_one(request)
+    return transition_one(request)
 
