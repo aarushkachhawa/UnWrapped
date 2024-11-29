072a0c8c6f244f19f7bae589728fd91b5beda4c2
Anonymoustank <pkadekodi@gmail.com>
2024-11-28 21:51:19 -0500

    made a halloween transition two page

--- UnWrapped/views.py
+++ UnWrapped/views.py
@@ -955,16 +955,20 @@ def christmas_transition_one(request):
         return redirect('home')
 
 @login_required
-def transition_two(request):
+def transition_two(request, page = "transitionTwo.html"):
     if 'spotify_access_token' not in request.session:
         return redirect(spotify_auth_url())
     try:
-        return render(request, 'transitionTwo.html', {'language': request.session.get('language', 'english')})
+        return render(request, page, {'language': request.session.get('language', 'english')})
 
     except Exception as e:
         logger.error(f"Error in transition view: {e}")
         messages.error(request, "An error occurred while loading the transition page.")
         return redirect('home')
+
+@login_required
+def halloween_transition_two(request):
+    return transition_two(request, "halloween_TransitionTwo.html")
 
 def calculate_get_account_level(request):
     access_token = request.session.get('spotify_access_token')
